38e61ee29b562f51ec4f7a373b887f4e9e67ef1e
sergeymaguire <sergeymaguire@gmail.com>
2018-08-29 13:36:07 -0700

    updated homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,27 +34,29 @@ var darthMaul = {
     attack: 25
 };
 let players = [obiWanKenobi, lukeSkywalker, darthSidious, darthMaul];
+var currentPlayerIndex = 0;
+
 $("#lukeskywalker").on("click", function () {
     console.log("Luke Skywalker Selected");
-    showCharacter(1);
-    playerSelection(1);
-
-});
-$("#obiwan").on("click", function () {
-    console.log("Obi-Wan Kenobi Selected");
     showCharacter(2);
     playerSelection(2);
 
 });
+$("#obiwan").on("click", function () {
+    console.log("Obi-Wan Kenobi Selected");
+    showCharacter(1);
+    playerSelection(1);
+
+});
 $("#darthmaul").on("click", function () {
     console.log("Darth Maul Selected");
-    showCharacter(3);
-    playerSelection(3);
+    showCharacter(4);
+    playerSelection(4);
 });
 $("#darthsidious").on("click", function () {
     console.log("Darth Sidious Selected");
-    showCharacter(4);
-    playerSelection(4);
+    showCharacter(3);
+    playerSelection(3);
 });
 $("#attack").on("click", function () {
     console.log("Attack Button Selected");
@@ -66,52 +68,57 @@ $("#restart").on("click", function () {
 });
 $("#lukeskywalker3").on("click", function () {
     console.log("luke skywalker enemy clicked");
-    hideEnemiesExcept(1);
+    hideEnemiesExcept(2);
 });
 $("#obiwan3").on("click", function () {
     console.log("obiwan enemy clicked");
-    hideEnemiesExcept(2);
+    hideEnemiesExcept(1);
 });
 $("#darthmaul3").on("click", function () {
     console.log("darth maul enemy clicked");
-    hideEnemiesExcept(3);
+    hideEnemiesExcept(4);
 });
 $("#darthsidious3").on("click", function () {
     console.log("darthsidious enemy clicked");
-    hideEnemiesExcept(4);
+    hideEnemiesExcept(3);
 });
 
 function playerSelection(hidePlayers) {
     if (hidePlayers !== 1)
-        $("#lukeskywalker").hide().parent().hide();
-    if (hidePlayers !== 2)
         $("#obiwan").hide().parent().hide();
+
+    if (hidePlayers !== 2)
+        $("#lukeskywalker").hide().parent().hide();
+
     if (hidePlayers !== 3)
-        $("#darthmaul").hide().parent().hide();
-    if (hidePlayers !== 4)
         $("#darthsidious").hide().parent().hide();
+
+    if (hidePlayers !== 4)
+        $("#darthmaul").hide().parent().hide();
+
 };
 
 function showCharacter(characterToShow) {
+    currentPlayerIndex = characterToShow - 1;
     if (characterToShow === 1)
-        $("#lukeskywalker3").hide().parent().hide();
-    else
-        $("#lukeskywalker2").hide().parent().hide();
-
-    if (characterToShow === 2)
         $("#obiwan3").hide().parent().hide();
     else
         $("#obiwan2").hide().parent().hide()
 
-    if (characterToShow === 3)
-        $("#darthmaul3").hide().parent().hide();
+    if (characterToShow === 2)
+        $("#lukeskywalker3").hide().parent().hide();
     else
-        $("#darthmaul2").hide().parent().hide()
+        $("#lukeskywalker2").hide().parent().hide();
 
-    if (characterToShow === 4)
+    if (characterToShow === 3)
         $("#darthsidious3").hide().parent().hide();
     else
         $("#darthsidious2").hide().parent().hide()
+
+    if (characterToShow === 4)
+        $("#darthmaul3").hide().parent().hide();
+    else
+        $("#darthmaul2").hide().parent().hide()
 };
 
 function hideYourEnemiesToAttack() {
@@ -123,41 +130,42 @@ function hideYourEnemiesToAttack() {
 
 function hideEnemiesExcept(enemyToShow) {
     if (enemyToShow === 1) {
-        $("#lukeskywalker3").hide().parent().hide();
-        $("#lukeskywalker4").show().parent().show();
-        
-         $("#obiwan4").hide().parent().hide();
-         $("#darthmaul4").hide().parent().hide();
-         $("#darthsidious4").hide().parent().hide();
+        $("#obiwan3").hide().parent().hide();
+        $("#obiwan4").show().parent().show();
+
+        $("#lukeskywalker4").hide().parent().hide();
+        $("#darthmaul4").hide().parent().hide();
+        $("#darthsidious4").hide().parent().hide();
         return;
     };
 
     if (enemyToShow === 2) {
-        $("#obiwan3").hide().parent().hide();
-        $("#obiwan4").show().parent().show();
-        
-         $("#lukeskywalker4").hide().parent().hide();
-         $("#darthmaul4").hide().parent().hide();
-         $("#darthsidious4").hide().parent().hide();
+        $("#lukeskywalker3").hide().parent().hide();
+        $("#lukeskywalker4").show().parent().show();
+
+        $("#obiwan4").hide().parent().hide();
+        $("#darthmaul4").hide().parent().hide();
+        $("#darthsidious4").hide().parent().hide();
         return;
-        
+
     };
     if (enemyToShow === 3) {
-        $("#darthmaul3").hide().parent().hide();
-        $("#darthmaul4").show().parent().show();
-        
-         $("#lukeskywalker4").hide().parent().hide();
-         $("#obiwan4").hide().parent().hide();
-         $("#darthsidious4").hide().parent().hide();
+        $("#darthsidious3").hide().parent().hide();
+        $("#darthsidious4").show().parent().show();
+       
+
+        $("#lukeskywalker4").hide().parent().hide();
+        $("#obiwan4").hide().parent().hide();
+        $("#darthmaul4").hide().parent().hide();
         return;
     };
     if (enemyToShow === 4) {
-        $("#darthsidious3").hide().parent().hide();
-        $("#darthsidious4").show().parent().show();
-        
-         $("#lukeskywalker4").hide().parent().hide();
-         $("#obiwan4").hide().parent().hide();
-         $("#darthmaul4").hide().parent().hide();
+        $("#darthmaul3").hide().parent().hide();
+        $("#darthmaul4").show().parent().show();
+
+        $("#lukeskywalker4").hide().parent().hide();
+        $("#obiwan4").hide().parent().hide();
+        $("#darthsidious4").hide().parent().hide();
         return;
     };
 };
@@ -165,18 +173,13 @@ function hideEnemiesExcept(enemyToShow) {
 function gameRestart() {
     location.reload();
 };
-// function hide (){
-//     $("#lukeskywalker2").hide().parent().hide();
-//     $("#lukeskywalker3").hide().parent().hide();
-//     $("#lukeskywalker4").hide().parent().hide();
-//     $("#obiwan2").hide().parent().hide();
-//     $("#obiwan3").hide().parent().hide();
-//     $("#obiwan4").hide().parent().hide();
-//     $("#darthmaul2").hide().parent().hide();
-//     $("#darthmaul3").hide().parent().hide();
-//     $("#darthmaul4").hide().parent().hide();
-//     $("#darthsidious2").hide().parent().hide();
-//     $("#darthsidious3").hide().parent().hide();
-//     $("#darthsidious4").hide().parent().hide();
-// };
-// hide();
+
+function winner() {
+    gameRestart();
+    alert("Winner winner chicken dinner")
+};
+
+function loser() {
+    gameRestart();
+    alert("Loser loser double loser")
+};
